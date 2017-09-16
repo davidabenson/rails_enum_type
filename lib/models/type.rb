c44f87055ac43c5916917309b50781b62484745e
Dave Benson <david_a_benson@yahoo.com>
2017-09-15 18:05:47 -0600

    Complete wiring it up

--- lib/models/type.rb
+++ lib/models/type.rb
@@ -5,25 +5,16 @@ class Type < ActiveRecord::Base
   accepts_nested_attributes_for :type_items, :reject_if => :all_blank, :allow_destroy => true
   
   # VALIDATION
-  validates :name, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+  validates :description, length: { maximum: 255 }
   
-  def self.find_by_type_name(type_name)
-    this.Type.find_by_name(type_name).type_items
+  def self.find_type_items_by_name(type_name)
+    find_by_name(type_name).type_items
   end
 
   def self.find_type_item_by_name(key, name)
-    items = Type.find_by_name(key).type_items.select { |i| i.name == name }
+    items = find_by_name(key).type_items.select { |i| i.name == name }
     items.try(:first)
-  end
-  
-  def self.reorder_by_name(type_name)
-     items = Type.find_by_name(type_name).type_items.order(:name)
-     idx = 1
-     items.each do |gti|
-       gti.sequence = idx
-       gti.save
-       idx += 1
-     end
   end
 end
 
--- lib/models/type_item.rb
+++ lib/models/type_item.rb
@@ -3,5 +3,6 @@ class TypeItem < ActiveRecord::Base
   belongs_to :type
   
   # VALIDATION
-  validates :name, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :description, length: { maximum: 255 }
 end
