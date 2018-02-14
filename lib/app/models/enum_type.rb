944a19277e46aa197b175590f58328a776b4ab66
Dave Benson <david.benson@nrel.gov>
2018-02-14 13:57:18 -0700

    add to_json back in

--- lib/app/models/enum_type.rb
+++ lib/app/models/enum_type.rb
@@ -1,7 +1,13 @@
 module ET
   class EnumType < ET::TypeItem
 
+    class << self;
+      class_attribute :type_name
+
+    end
+
     def self.load_type_items(type_name, definer_method, order=:name)
+      @type_name = type_name
       items = ET::Type.find_by_type_name(type_name).order(order)
       items.each do |item|
 
@@ -13,6 +19,18 @@ module ET
 
     def symbol
       self.name.parameterize.underscore
+    end
+
+    def self.to_json()
+      a = []
+      puts "@type_name: #{EnumType.type_name}"
+      items = Model::Type.find_by_type_name(EnumType.type_name)
+      puts items
+      items.each do |enum_type|
+        a << {id: enum_type.id, name: enum_type.name}
+      end
+
+      return a.to_json.html_safe
     end
 
   end
