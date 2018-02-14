55e80c8bbf92ce442487fed78fb4297e8f4b68cc
Dave Benson <david.benson@nrel.gov>
2018-02-14 11:37:24 -0700

    Refactor out enum_item.rb, use Model object instead

--- lib/app/models/color_type.rb
+++ lib/app/models/color_type.rb
@@ -5,7 +5,7 @@ class ColorType < ET::EnumType
   class << self;
     class_attribute :type_name
     self.type_name = TYPE_NAME
-    ET::EnumType.load_type_items(self.type_name, definer_method = method(:define_method))
+    ET::EnumType.load_type_items(self.type_name, definer_method = method(:define_method), order=:sequence)
   end
 
   default_scope { where(type_id: ET::Type.find_by_name(self.type_name).id) }
