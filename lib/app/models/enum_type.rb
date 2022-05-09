8b7cbeceb54d7c1c2ed102b862180f402786c28f
Benson, David <David.Benson@nrel.gov>
2022-05-09 09:57:24 -0700

    update dependancies

--- lib/app/models/enum_type.rb
+++ lib/app/models/enum_type.rb
@@ -2,6 +2,7 @@ module ET
   class EnumType < ET::TypeItem
 
     def self.load_type_items(type_name, definer_method, order=:name)
+      puts "!!!!!ET::TypeItem:load_type_items: here"
       items = ET::Type.find_by_type_name(type_name).order(order)
       items.each do |item|
         definer_method.call(item.name.parameterize.underscore) do
@@ -9,6 +10,15 @@ module ET
         end
       end
     end
+
+    # def self.typed_load_type_items(items, type_name, definer_method, order=:name)
+    #   puts "!!!!!ET::TypeItem:typed_load_type_items: here"
+    #   items.each do |item|
+    #     definer_method.call(item.name.parameterize.underscore) do
+    #       return item
+    #     end
+    #   end
+    # end
 
     def symbol
       self.name.parameterize.underscore
